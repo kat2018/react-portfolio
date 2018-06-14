0caac7b205f9abdadc2f94a2eb97203e320bc30d
kat2018 <kat.vge@gmail.com>
2018-06-14 11:05:54 -0400

    created Specialties

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Profile from './components/Profile';
 
+// If it's a class name, it must be capitalized. (Example: App, Component and Profile ^)
 class App extends Component {
   render() {
     return (
---
+++ src/components/Specialties.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+
+class Specialties extends Component {
+    render (){
+        return (
+            <div>Specialties Component</div>
+        )
+    }
+}
+export default Specialties
