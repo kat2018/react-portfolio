e4e726bf533f53af549938efd53af3b12c40cefd
kat2018 <kat.vge@gmail.com>
2018-06-14 11:08:01 -0400

    Created Specialties Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Profile from './components/Profile';
+import Specialties from './components/Specialties';
 
 // If it's a class name, it must be capitalized. (Example: App, Component and Profile ^)
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Profile />
+        <Specialties />
       </div>
     );
   }
