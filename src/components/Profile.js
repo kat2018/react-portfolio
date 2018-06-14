a2c366e0b22b2793d7f476b4e24b57af6d5d7ffc
kat2018 <kat.vge@gmail.com>
2018-06-14 11:03:08 -0400

    Nic on page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,12 @@ import React, { Component } from "react"
 class Profile extends Component{
     render(){
         return(
-            <div>Profile</div>
+            <div>
+                <img src="http://www.placecage.com/200/300" />
+                <h1>Nic Cage</h1>
+                <h3>Full Stack Developer</h3>
+            </div>
+            
         )
     }
 }
