32e3c69e4462385d937084380ef0c9ce3021769e
kat2018 <kat.vge@gmail.com>
2018-06-14 11:18:41 -0400

    Created Specialty Component

--- src/components/Specialties.js
+++ src/components/Specialties.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react"
+import Specialty from "./Specialty";
 
 class Specialties extends Component {
     render (){
-        return (
-            <div>Specialties Component</div>
-        )
+        return <div>
+            <h1>I specialize in ...</h1>
+            <Specialty />
+            <Specialty />
+            <Specialty />
+          </div>;
     }
 }
 export default Specialties
---
+++ src/components/Specialty.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react"
+
+class Specialty extends Component {
+    render() {
+        return (
+            <div>
+                Specialties Component
+
+                Specialty 1
+                 JavaScript
+             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/2000px-Unofficial_JavaScript_logo_2.svg.png" alt="JavaScript Image" width="200" />
+             
+             Description: 
+             JavaScript is a programming language commonly used in web development. It was originally developed by Netscape as a means to add dynamic and interactive elements to websites. While JavaScript is influenced by Java, the syntax is more similar to C and is based on ECMAScript, a scripting language developed by Sun Microsystems.
+            </div>
+        )
+    }
+}
+
+export default Specialty
